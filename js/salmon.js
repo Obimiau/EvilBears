1a7d2974ee4a274d15eb7af0968cf2817da9fdee
Maria Ruiz <mariaruizestevez@gmail.com>
2017-10-31 15:23:12 +0100

    Life function - work in progress

--- js/salmon.js
+++ js/salmon.js
@@ -3,6 +3,7 @@ function Salmon (x, y, board) {
   this.xPosition = x;
   this.yPosition = y;
   this.board = board;
+  this.life = 40;
 
   this.template = document.createElement('div');
   this.template.className = "salmon";
@@ -39,4 +40,30 @@ Salmon.prototype.updatePos = function() {
     this.move();
   }.bind(this), 500);
 
+};
+
+//Trabajando
+
+Salmon.prototype.receiveDamage = function() {
+  var x = this.xPosition;
+  var y = this.yPosition;
+  this.board[x][y] = 'X';
+
+  if(this.board[x-1] != null && this.board[x-1][y] === 'B') {
+    this.life = this.life-10;
+  } else if (this.board[x][y+1] != null && this.board[x][y+1] === 'B') {
+    this.life = this.life-10;
+  } else if (this.board[x+1] != null && this.board[x+1][y] === 'B') {
+    this.life = this.life-10;
+  } else if (this.board[x][y-1] != null && this.board[x][y-1] === 'B') {
+    this.life = this.life-10;
+  }
+};
+
+Salmon.prototype.checkDeath = function (damage) {
+  this.life -= damage;
+  if (this.life>0){ //continúa moviéndose
+  } else {
+    //actualizar vida
+  }
 };
