cef5435d7346bc48512f3eca5f44575298492695
Maria Ruiz <mariaruizestevez@gmail.com>
2017-10-31 13:48:59 +0100

    added click for bears

--- js/bear.js
+++ js/bear.js
@@ -1,5 +1,14 @@
-var bear = [];
+var bears = [];
 
-function Bears (){
+function Bear (x,y,board){
+  this.xPosition = x;
+  this.yPosition = y;
+  this.board = board;
+};
 
-}
+
+Bear.prototype.attack = function() {
+  var x = this.xPosition;
+  var y = this.yPosition;
+  this.board[x][y] = 'X';
+};
--- js/board.js
+++ js/board.js
@@ -11,25 +11,46 @@ var board = [
   ['G','G','G','G','G','G','G','G','G','G'],
   ];
 
-var grass = '<div class="grass square"></div>';
-var river = '<div class="river square"></div>';
+  var bearCounter = 0;
+  var b = document.querySelector(".riverBoard");
 
-var boardHTML = '';
-
-//For para generar el background
-for(var x = 0; x < board.length; x++) {
-  boardHTML = boardHTML + '<div class="row">';
-  for(var y = 0; y < board[x].length; y++) {
-      switch(board[x][y]) {
-        case 'G':
-          boardHTML = boardHTML + grass;
-          break;
-        case 'R':
-          boardHTML = boardHTML + river;
+  drawBoard = function() {
+    // PONGO LOS DATA-X Y DATA-Y PARA QUE LUEGO AL CLICKAR SEPA DONDE ESTÁ EL CUADRADO
+  var row = '';
+  for(var x = 0; x < board.length; x++) {
+    row = row + '<div class="row">';
+    for(var y = 0; y < board[x].length; y++) {
+      if(board[x][y] === 'G') {
+        row = row + '<div class="square grass" data-x="'+ x +'"  data-y="'+ y +'"></div>';
+      } else if(board[x][y] === 'R' || board[x][y] === 'X') {
+        row = row + '<div class="square river" data-x="'+ x +'"  data-y="'+ y +'"></div>';
+      } else {
+        row = row + '<div class="square" data-x="'+ x +'"  data-y="'+ y +'"><div class="bear"></div></div>';
       }
-  }
-  boardHTML = boardHTML + '</div>';
-};
+    }
+    row = row + '</div>';
+  };
 
-document.querySelector('.riverBoard').innerHTML = boardHTML;
-  var jon = new Salmon(1,0, board);
+   b.innerHTML = row;
+
+     document.querySelectorAll('.grass').forEach( function(el) {
+       el.onclick = function(ev) {
+         // recojo el evento de click, y accedo al elemento que he pinchado con target
+        var element = ev.target;
+        // ahora puedo acceder a donde esta gracias a los data
+         var x = element.getAttribute('data-x');
+         var y = element.getAttribute('data-y');
+
+         // ahora, con x e y, cambio la letra de array a B de Bear
+         board[x][y] = 'B';
+         bearCounter = bearCounter + 1;
+         // como el array ha cambiado, tengo que volver a llamar a la funcion para que pinte de nuevo el mapa
+         drawBoard();
+        // ahora ya puedo hacer, que cada vez que el salmon se mueve, si alrededor tengo un 'B' la vida del salmon baje.
+      };
+     });
+   };
+
+    drawBoard();
+
+  var salmon1 = new Salmon(1,0, board);
